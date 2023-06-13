50e61d63f69ed4a96171646a9f4ff6acf228a061
DaFei <deFang@users.noreply.github.com>
2023-06-13 15:47:08 +0800

    update

--- src/components/Chain.js
+++ src/components/Chain.js
@@ -1,4 +1,4 @@
-import {ethers} from 'ethers'
+import { ethers } from 'ethers'
 
 export const ONE = ethers.utils.parseEther('1')
 export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
@@ -22,16 +22,16 @@ export const PROVIDERS = {
     Zksync: new ethers.providers.JsonRpcProvider('https://mainnet.era.zksync.io'),
     Polygon: new ethers.providers.JsonRpcProvider('https://polygon-rpc.com/'),
     Goerli: new ethers.providers.JsonRpcProvider('https://dry-solemn-lambo.ethereum-goerli.quiknode.pro/5143675172d55e2d8866d16c6f177a6adb4d8466/'),
-    BscTestnet: new ethers.providers.JsonRpcProvider('https://data-seed-prebsc-1-s3.binance.org:8545/'),
+    BscTestnet: new ethers.providers.JsonRpcProvider('https://data-seed-prebsc-1-s2.binance.org:8545'),
     ArbitrumTestnet: new ethers.providers.JsonRpcProvider('https://goerli-rollup.arbitrum.io/rpc'),
     // Localhost: new ethers.providers.JsonRpcProvider('http://127.0.0.1:8545'),
 }
 
-export const bb = (value, decimals=18) => {
+export const bb = (value, decimals = 18) => {
     return value && ethers.utils.parseUnits(value.toString(), decimals)
 }
 
-export const nn = (value, decimals=18) => {
+export const nn = (value, decimals = 18) => {
     return value && ethers.utils.formatUnits(value, decimals)
 }
 
@@ -41,13 +41,13 @@ export const connectMetaMask = async () => {
     await provider.send('eth_requestAccounts', [])
 }
 
-export const executeTx = async (func, args=[]) => {
+export const executeTx = async (func, args = []) => {
     try {
         return await func(...args)
     } catch (error) {
         const message = `Transaction will fail with reason:\n${error?.reason || error?.message || error}\n\nWould you like to send the transaction anyway?`
         if (window.confirm(message)) {
-            return await func(...args, {gasLimit: 1000000})
+            return await func(...args, { gasLimit: 1000000 })
         }
     }
 }
